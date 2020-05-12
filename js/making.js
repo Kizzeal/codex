7982f9c60b8fd4c63cb8b8fd73927b919eae2d0c
Kizzeal <chukwudiobi6@gmail.com>
2020-05-12 16:54:57 +0100

    gsap animation

---
+++ js/making.js
@@ -0,0 +1,8 @@
+document.getElementById("cta").addEventListener("click", () => {
+  let tl = gsap.timeline({ defaults: { duration: 1 } });
+
+  tl.to(".panel", { scaleY: 1, height: "100vh", ease: Power3.inOut })
+    .to("#pdf", { opacity: 1, y: 0, ease: "bounce.out" }, "+=0.2")
+    .to("p", { opacity: 1, y: 20 }, "-=0.1")
+    .to(".box", { opacity: 1, scale: 1, ease: "elastic(0.5, 0.3)" }, 0.5);
+});
