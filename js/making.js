de97cd1ac6091d6ed1d97dcb79f858e439ba45ae
Kizzeal <chukwudiobi6@gmail.com>
2020-05-13 13:30:53 +0100

    adjusted the gsap animation slightly

--- js/making.js
+++ js/making.js
@@ -1,8 +1,9 @@
 document.getElementById("cta").addEventListener("click", () => {
   let tl = gsap.timeline({ defaults: { duration: 1 } });
 
-  tl.to(".panel", { scaleY: 1, height: "100vh", ease: Power3.inOut })
+  tl.to(".shift", { x: "-30px", ease: "bounce.out" })
+    .to(".panel", { scaleY: 1, height: "100vh", ease: Power3.inOut })
     .to("#pdf", { opacity: 1, y: 0, ease: "bounce.out" }, "+=0.2")
     .to("p", { opacity: 1, y: 20 }, "-=0.1")
-    .to(".box", { opacity: 1, scale: 1, ease: "elastic(0.5, 0.3)" }, 0.5);
+    .to(".box", { opacity: 1, scale: 1, ease: "elastic(0.5, 0.3)" }, 2);
 });
