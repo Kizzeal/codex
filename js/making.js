6fcb8ec01dac05c4b1d6327e36a92ecb9110cc8c
Kizzeal <chukwudiobi6@gmail.com>
2020-05-16 17:04:12 +0100

    made responsive changes to the grid.html & added an small piece of animation to the gsap

--- js/making.js
+++ js/making.js
@@ -1,9 +1,10 @@
 document.getElementById("cta").addEventListener("click", () => {
   let tl = gsap.timeline({ defaults: { duration: 1 } });
 
-  tl.to(".shift", { x: "-30px", ease: "bounce.out" })
+  tl.to(".shift", { x: "800px", ease: "power2.inOut" })
+    .to(".shift", { x: "-30px", ease: "bounce.out" })
     .to(".panel", { scaleY: 1, height: "100vh", ease: Power3.inOut })
     .to("#pdf", { opacity: 1, y: 0, ease: "bounce.out" }, "+=0.2")
     .to("p", { opacity: 1, y: 20 }, "-=0.1")
-    .to(".box", { opacity: 1, scale: 1, ease: "elastic(0.5, 0.3)" }, 2);
+    .to(".box", { opacity: 1, scale: 1, ease: "elastic(0.5, 0.3)" }, 3);
 });
