be7b20965ef757cdc7b1fad4af4fdd4b3d62da9c
Kizzeal <chukwudiobi6@gmail.com>
2020-07-21 09:35:57 +0100

    adding gsap files to master branch adding first.html(my online portfolio) to master branch

--- js/landing.js
+++ js/landing.js
@@ -5,48 +5,45 @@ let z = document.getElementById("btn");
 let form = document.getElementById("form-box");
 
 login = () => {
-    x.style.left = "50px";
-    y.style.left = "450px";
-    z.style.left = "0px";
-}
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+};
 register = () => {
-    x.style.left = "-450px";
-    y.style.left = "50px";
-    z.style.left = "110px";
-}
+  x.style.left = "-450px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+};
 // forgot = () => {
 //     form.style.display = "none";
 // }
-
 
 let random = document.getElementById("punch");
 let randomizer = document.getElementById("punch-btn");
 let rand = document.getElementById("img1");
 let randImg = document.getElementById("img2");
 let images = [
-    "images/pexels.jpeg",
-    "images/nero_devil_may_cry_5_4k-1920x1080.jpg",
-    "images/darksiders_war_horse_fire_city_21821_1920x1200_2.jpg",
-    "images/3d_bars-1366x768.jpg",
-    "images/yosemiti_valley_4k_8k-3840x2161.jpg",
-    "images/titanfall_game_heroes_robot_92981_1920x1080.jpg",
-    "images/Sunrise.jpg",
-    "images/portfolio11.jpg",
-    "images/mazda3_tcr_2019_4k_8k_2-1920x1080.jpg"
+  "images/pexels.jpeg",
+  "images/nero_devil_may_cry_5_4k-1920x1080.jpg",
+  "images/darksiders_war_horse_fire_city_21821_1920x1200_2.jpg",
+  "images/3d_bars-1366x768.jpg",
+  "images/yosemiti_valley_4k_8k-3840x2161.jpg",
+  "images/titanfall_game_heroes_robot_92981_1920x1080.jpg",
+  "images/Sunrise.jpg",
+  "images/portfolio11.jpg",
+  "images/mazda3_tcr_2019_4k_8k_2-1920x1080.jpg",
 ];
 
-
 random.addEventListener("click", () => {
-    let randNum = Math.floor(Math.random() * images.length)
+  let randNum = Math.floor(Math.random() * images.length);
 
-    rand.src = images[randNum];
+  rand.src = images[randNum];
 });
 
-
 randomizer.addEventListener("click", () => {
-    let randNum = Math.floor(Math.random() * images.length)
+  let randNum = Math.floor(Math.random() * images.length);
 
-    randImg.src = images[randNum];
+  randImg.src = images[randNum];
 });
 
 /* Math.ceil(Math.random()*10)
--- js/making.js
+++ js/making.js
@@ -1,10 +1,12 @@
 document.getElementById("cta").addEventListener("click", () => {
   let tl = gsap.timeline({ defaults: { duration: 1 } });
 
-  tl.to(".shift", { x: "800px", ease: "power2.inOut" })
+  tl.to(".shift button", { color: "black" }, "-=2")
+    .to(".shift", { x: "600px", ease: "power2.inOut" })
     .to(".shift", { x: "-30px", ease: "bounce.out" })
+    .to(".shift h1", { color: "black" })
     .to(".panel", { scaleY: 1, height: "100vh", ease: Power3.inOut })
     .to("#pdf", { opacity: 1, y: 0, ease: "bounce.out" }, "+=0.2")
     .to("p", { opacity: 1, y: 20 }, "-=0.1")
-    .to(".box", { opacity: 1, scale: 1, ease: "elastic(0.5, 0.3)" }, 3);
+    .to(".box", { opacity: 1, scale: 1, ease: "elastic(0.5, 0.3)" }, 5.5);
 });
